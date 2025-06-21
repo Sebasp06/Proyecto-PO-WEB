5acc94dbee94af7df88151b5aa7434e92cdc5039
Sebasp-06 <saperezs.24@est.ucab.edu.ve>
2025-06-21 15:40:34 -0400

    feat
    Estableciendo la logica del js, especificamente en las funciones para inicializar el mazo, proximamente el repartir cartas

--- uno-web-edition/table/script.js
+++ uno-web-edition/table/script.js
@@ -2,3 +2,51 @@ let deck = [];
 let discardPile = [];
 const colors = ['red','green','blue','yellow'];
 const specialCards = ['jump','reverse','draw2'];
+
+function initializeDeck(){
+    
+
+    for(let color = 0; color < 4; color ++){
+        const card = {
+            id: colors[color] + '-0',
+            color: colors[color],
+            type: 'number',
+            value: 0
+    
+        };
+        deck.push(card);
+        for(let number = 1; number < 10; number++){
+
+            const card = {
+                id: colors[color] + '-' + number.toString(),
+                color: colors[color],
+                type: 'number',
+                value: number
+        
+            };
+            //preguntar a la profe respecto a esto
+            /*
+            const card2 = {
+                id: colors[color] + '-' + number.toString(),
+                color: colors[color],
+                type: 'number',
+                value: number
+        
+            };
+            
+            deck.push(card2);
+            */
+            deck.push(card);
+        }
+        
+    }
+    console.log(deck);
+    
+}
+
+function dealCards(){
+    let playerDeck = document.getElementById('player-deck');
+    
+}
+
+initializeDeck();
